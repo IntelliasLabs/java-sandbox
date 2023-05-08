2e67978007ce3aed7490832c5e9d3474fe171733
i-vasylyk <iryna.r.vasylyk@intellias.com>
2023-05-08 11:00:46 +0300

    adjusted validation

--- golden-example/src/main/java/com/intellias/basicsandbox/controller/ItemController.java
+++ golden-example/src/main/java/com/intellias/basicsandbox/controller/ItemController.java
@@ -2,12 +2,11 @@ package com.intellias.basicsandbox.controller;
 
 import com.intellias.basicsandbox.service.ItemService;
 import com.intellias.basicsandbox.service.dto.ItemDTO;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,18 +20,18 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    public ResponseEntity<ItemDTO> save(@Validated @RequestBody final ItemDTO item) {
+    public ResponseEntity<ItemDTO> save(@Valid @RequestBody final ItemDTO item) {
         return new ResponseEntity<>(itemService.save(item), HttpStatus.CREATED);
     }
 
     @PutMapping
-    public ResponseEntity<Void> update(@Validated @RequestBody final ItemDTO item) {
+    public ResponseEntity<Void> update(@Valid @RequestBody final ItemDTO item) {
         itemService.update(item);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ItemDTO> getById(@NotNull @PathVariable("id") final UUID id) {
+    public ResponseEntity<ItemDTO> getById(@PathVariable("id") final UUID id) {
         return new ResponseEntity<>(itemService.getById(id), HttpStatus.OK);
     }
 
@@ -42,7 +41,7 @@ public class ItemController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@NotNull @PathVariable("id") final UUID id) {
+    public ResponseEntity<Void> delete(@PathVariable("id") final UUID id) {
         itemService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
