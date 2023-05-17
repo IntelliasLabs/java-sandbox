69c902b5862ec969b77eca819bd22af9409c98d8
Andrii Kasianenko <kxaxsx@gmail.com>
2023-05-17 11:29:39 +0300

    Add logging config for controllers

--- golden-example/src/main/java/com/intellias/basicsandbox/controller/ItemController.java
+++ golden-example/src/main/java/com/intellias/basicsandbox/controller/ItemController.java
@@ -6,6 +6,7 @@ import com.intellias.basicsandbox.controller.dto.ItemDTO;
 import com.intellias.basicsandbox.controller.mapper.ItemMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping(ItemController.API_VERSION + ItemController.PATH)
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Slf4j
 public class ItemController {
     public final static String PATH = "items";
     public final static String API_VERSION = "/api/v1/";
@@ -28,13 +30,17 @@ public class ItemController {
 
     @PostMapping
     public ResponseEntity<ItemDTO> save(@Valid @RequestBody final ItemDTO item) {
+        log.debug("Item to save: {}", item);
         ItemEntity savedItem = itemService.save(itemMapper.toEntity(item));
+        log.info("Item successfully saved: {}", savedItem);
         return new ResponseEntity<>(itemMapper.toDTO(savedItem), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ItemDTO> update(@PathVariable("id") final UUID id, @Valid @RequestBody final ItemDTO item) {
+        log.debug("Item with id {} to update: {}", id, item);
         ItemEntity updatedItem = itemService.update(id, itemMapper.toEntity(item));
+        log.info("Item with id {} successfully updated: {}", id, updatedItem);
         return new ResponseEntity<>(itemMapper.toDTO(updatedItem), HttpStatus.OK);
     }
 
@@ -51,12 +57,15 @@ public class ItemController {
                 .map(itemMapper::toDTO)
                 .toList();
 
+        log.info("Items found: {}", items);
         return new ResponseEntity<>(items, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") final UUID id) {
+        log.debug("Item with id {} to delete", id);
         itemService.delete(id);
+        log.info("Item with id {} successfully deleted", id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
