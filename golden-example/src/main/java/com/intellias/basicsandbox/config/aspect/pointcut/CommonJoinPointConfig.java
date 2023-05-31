2bab955bdb71ab9a44a1c266d6d0720467056d07
Andrii Kasianenko <kxaxsx@gmail.com>
2023-05-31 10:34:57 +0300

    Add comments to Logging aspects.
    Refactor to use @ConditionalOnProperty instead of flag

--- golden-example/src/main/java/com/intellias/basicsandbox/config/aspect/pointcut/CommonJoinPointConfig.java
+++ golden-example/src/main/java/com/intellias/basicsandbox/config/aspect/pointcut/CommonJoinPointConfig.java
@@ -4,6 +4,9 @@ package com.intellias.basicsandbox.config.aspect.pointcut;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+/**
+ * Config for enabling pointcut on @Service annotated classes
+ */
 @Component
 public class CommonJoinPointConfig {
 
