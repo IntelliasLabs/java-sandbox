ec2ee4d6073280352839c3968b2206b8db1a5563
Anton Nakonechnyi <anton.nak@gmail.com>
2023-06-20 11:37:36 +0300

    add empty constructor for ErrorDTO

--- src/main/java/com/intellias/basicsandbox/controller/dto/ErrorDTO.java
+++ src/main/java/com/intellias/basicsandbox/controller/dto/ErrorDTO.java
@@ -2,9 +2,11 @@ package com.intellias.basicsandbox.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorDTO {
     private HttpStatus status;
